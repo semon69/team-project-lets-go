e16d5cd5bec8b041454d57567a425d7372110dc9
semon69 <smdemon3@gmail.com>
2023-09-11 15:33:41 +0600

    add next/dynamic importer in bokking page

--- src/app/bookings/page.js
+++ src/app/bookings/page.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import BookingSlider from '../../components/Bookings/BookingSlider/BookingSlider';
-import Discover from '../../components/Bookings/Discover/Discover';
-import Rating from '../../components/Bookings/Rating/Rating';
-import Explore from '@/components/Bookings/Explore/Explore';
+// import BookingSlider from '../../components/Bookings/BookingSlider/BookingSlider';
+// import Discover from '../../components/Bookings/Discover/Discover';
+// import Rating from '../../components/Bookings/Rating/Rating';
+// import Explore from '@/components/Bookings/Explore/Explore';
+import dynamic from 'next/dynamic';
+
+const BookingSlider = dynamic(()=> import('../../components/Bookings/BookingSlider/BookingSlider'), {ssr: false})
+const Rating = dynamic(()=> import('../../components/Bookings/Rating/Rating'), {ssr: false})
+const Explore = dynamic(()=> import('@/components/Bookings/Explore/Explore'), {ssr: false})
+const Discover = dynamic(()=> import('../../components/Bookings/Discover/Discover'), {ssr: false})
 
 
 const page = () => {
