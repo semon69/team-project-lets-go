5e169ae9f796cefedbf8ab88b284eec591046db9
semon69 <smdemon3@gmail.com>
2023-09-11 20:48:44 +0600

    add new favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         <ScrollToTop color="white" smooth />
         <Navbar />
         <div className="pt-20">
-        {children}
+          {children}
 
         </div>
 
